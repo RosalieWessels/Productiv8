24210db558a1f13ab8bef0c1f04d3e324fe42114
Rosalie Wessels <rosaliew@Franks-MacBook-Pro.local>
2018-10-13 09:37:41 -0700

    First commit

---
+++ Studify2.0/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Studify2.0
+//
+//  Created by Rosalie Wessels on 13/10/2018.
+//  Copyright © 2018 RosalieW. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
