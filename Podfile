691e6c165e4c3c147960545f3ce0f048d8f9dd1f
Rosalie Wessels <rosaliew@franks-macbook-pro.local>
2018-10-13 11:52:39 -0700

    Connected to Google Firebase

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Studify2.0' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Studify2.0
+pod 'Firebase/Core'
+end
---
+++ Studify2.0/SignUpViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpViewController.swift
+//  Studify2.0
+//
+//  Created by Rosalie Wessels on 13/10/2018.
+//  Copyright © 2018 RosalieW. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class SignUpViewContoller : UIViewController, UITextViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
