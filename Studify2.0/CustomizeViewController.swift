09069a0b20e8d83cd880a11c8b0fec430843a07f
Rosalie Wessels <Rosalie@rm01-lab-imac-06.local>
2019-04-12 11:45:53 -0700

    4/12

--- Studify2.0/CustomizeViewController.swift
+++ Studify2.0/CustomizeViewController.swift
@@ -10,9 +10,12 @@ import Foundation
 import Firebase
 import FirebaseAuth
 import FirebaseFirestore
+import FirebaseDatabase
+import UIKit
 
 class CustomizeViewController : UIViewController, UIGestureRecognizerDelegate {
     
+    @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var blueAndyellowImage: UIImageView!
     @IBOutlet weak var lightBlueAndOrange: UIImageView!
     @IBOutlet weak var lightBlueAndPink: UIImageView!
@@ -42,62 +45,97 @@ class CustomizeViewController : UIViewController, UIGestureRecognizerDelegate {
         let tapGestureRecognizer3 = UITapGestureRecognizer(target: self, action: #selector(lightBlueAndPinkImageTapped(tapGestureRecognizer:)))
         lightBlueAndPink.addGestureRecognizer(tapGestureRecognizer3)
         
-//        let UITapRecognizerblueAndYellowImage = UITapGestureRecognizer(target: self, action: Selector(("blueAndYellowImageTapped:")))
-//        UITapRecognizerblueAndYellowImage.delegate = self
-//        self.blueAndyellowImage.addGestureRecognizer(UITapRecognizerblueAndYellowImage)
-//        self.blueAndyellowImage.isUserInteractionEnabled = true
     }
     
     @objc func blueAndYellowImageTapped(tapGestureRecognizer: UITapGestureRecognizer){
         print("blueandyellowImageTapped")
         // Your action
         selectedBackgroundLabel.text = "Blue and Yellow Background is selected"
+        chosenBackground = "blueAndYellow"
+        uploadChosenThemeToFirebase()
     }
     
     @objc func lightBlueAndOrangeImageTapped(tapGestureRecognizer: UITapGestureRecognizer){
         print("lightBlueAndOrangeTapped")
         // Your action
         selectedBackgroundLabel.text = "Light Blue and Orange Background is selected"
+        chosenBackground = "lightBlueAndOrange"
+        uploadChosenThemeToFirebase()
     }
     
     @objc func lightBlueAndPinkImageTapped(tapGestureRecognizer: UITapGestureRecognizer){
         print("lightBlueAndPinkTapped")
         // Your action
         selectedBackgroundLabel.text = "Light Blue and Pink Background is selected"
+        chosenBackground = "lightBlueAndPink"
+        uploadChosenThemeToFirebase()
     }
     
     func uploadChosenThemeToFirebase() {
-    if let userEmail = Auth.auth().currentUser?.email {
-        let docRef = db.collection("customizeDatabase").document("\(userEmail)")
-        
-        docRef.getDocument { (document, error) in
-            if let document = document, document.exists {
-                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
-                print("Document data: \(dataDescription)")
-                
-                docRef.updateData([
-                    "background" : chosenBackground
-                ]) { err in
-                    if let err = err {
-                        print("Error updating document: \(err)")
-                    } else {
-                        print("Document successfully updated")
-                        self.generateScores()
+        if let userName = Auth.auth().currentUser?.displayName {
+            print("username : \(userName)")
+            let docRef = db.collection("customizeDatabase").document("\(userName)")
+            
+            docRef.getDocument { (document, error) in
+                if let document = document, document.exists {
+                    let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                    print("Document data: \(dataDescription)")
+                    
+                    docRef.updateData([
+                        "background" : self.chosenBackground
+                    ]) { err in
+                        if let err = err {
+                            print("Error updating document: \(err)")
+                        } else {
+                            print("Document successfully updated")
+                            self.reload()
+                        }
                     }
+                    
+                } else {
+                    print("Document does not exist")
+                    
+                    print("Creating Document")
+                    let newDocument = self.db.collection("customizeDatabase").document("\(userName)")
+                    
+                    newDocument.setData([
+                        "background": self.chosenBackground
+                        ])
+                    print("Document with CourseID created")
+                    
                 }
-                
-            } else {
-                print("Document does not exist")
-                
-                print("Creating Document")
-                let newDocument = self.db.collection("customizeDatabase").document("\()")
-                
-                newDocument.setData([
-                    "background": 0
-                    ])
-                print("Document with CourseID created")
-                self.generateScores()
-                
+            }
+        }
+    }
+    
+    func reload(){
+        if let userName = Auth.auth().currentUser?.displayName {
+            let docRef = db.collection("customizeDatabase").document("\(userName)")
+            docRef.getDocument { (document, error) in
+                if let document = document, document.exists {
+                    let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                    print("Document data: \(dataDescription)")
+                    
+                    if let background = document.get("background") as? String {
+                        print(background)
+                        if background == "lightBlueAndPink" {
+                            self.backgroundImageView.image = #imageLiteral(resourceName: "StudifyBackgroundLightBlue&Pink")
+                        }
+                        else if background == "lightBlueAndOrange" {
+                            self.backgroundImageView.image = #imageLiteral(resourceName: "StudifyBackgroundLightBlue&Orange")
+                        }
+                        else if background == "blueAndYellow" {
+                            self.backgroundImageView.image = #imageLiteral(resourceName: "StudifyBackground")
+                        }
+                        
+                        
+                    } else {
+                        print("No member Background")
+                    }
+                    
+                } else {
+                    print("No document with username")
+                }
             }
         }
     }
